e4b2495aa0d2b143993ff88937bd6e3bddf5cde9
Andres meza <amezadeveloper@gmail.com>
2025-02-16 19:37:22 -0500

    se termina session auth

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
+// import { RpcCustomExceptionFilter } from './../../client-gateway/src/common/exceptions/rpc-custom-exception.filter';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { RpcCustomExceptionFilter } from './common/exceptions';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3007);
+
+   const logger = new Logger('Auth-ms')
+  // const app = await NestFactory.create(AppModule);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    },
+  );
+
+  
+
+
+  
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    }),
+  ); 
+
+  app.useGlobalFilters(
+    new RpcCustomExceptionFilter()
+  )
+
+
+  await app.listen();
+  logger.log(`app running on port ${envs.port}`);
 }
 bootstrap();
